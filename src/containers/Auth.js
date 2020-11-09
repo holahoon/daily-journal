933cfe2e45ca19b1b41add2a31da5a2d87f10c5d
David Kim <hola.hoon@gmail.com>
2020-11-09 11:58:12 +0900

    saving work: authentication styling

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -1,28 +1,42 @@
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
 import {
+  makeStyles,
   Paper,
-  Grid,
+  Box,
   TextField,
   Button,
+  Typography,
   FormControlLabel,
-  Checkbox,
+  FormControl,
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+  IconButton,
 } from "@material-ui/core";
-import { Face, Fingerprint } from "@material-ui/icons";
+import {
+  Face,
+  Fingerprint,
+  PersonAdd,
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
 
 import { useAuthStateValue } from "hooks/context/AuthStateProvider";
 import useAuthState from "hooks/useState/useAuthState";
 import { authService } from "utils/firebaseInstance";
 
-function Auth(props) {
+function Auth() {
   const { userData } = useAuthStateValue()[0];
   const [form, onChangeHandler] = useAuthState({
     userName: "",
     email: "",
     password: "",
+    showPassword: false,
   });
   const [isNewAccount, setIsNewAccount] = useState(false);
   const [authError, setAuthError] = useState("");
+  const classes = useStyles();
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -58,51 +72,86 @@ function Auth(props) {
     setIsNewAccount((prev) => !prev);
   };
 
-  const { classes } = props;
-
   return (
     <>
       {userData ? (
         // Redirect the user to the main page if the user is already || has logged in || just signed up
         <Redirect to='/' />
       ) : (
-        <Paper>
-          <form onSubmit={onSubmitHandler}>
+        <Paper className={classes.paper}>
+          <FormControl autoComplete='off' fullWidth className={classes.form}>
             {isNewAccount && (
-              <input
+              <Box
+                display='flex'
+                alignItems='center'
+                className={classes.userNameBox}
+              >
+                <PersonAdd />
+                <TextField
+                  id='outlined-basic'
+                  label='User Name'
+                  variant='outlined'
+                  type='text'
+                  name='userName'
+                  value={form.userName}
+                  onChange={onChangeHandler}
+                />
+              </Box>
+            )}
+            <Box
+              display='flex'
+              alignItems='center'
+              className={classes.emailBox}
+            >
+              <Face />
+              <TextField
+                id='outlined-basic'
+                label='Email'
+                variant='outlined'
                 type='text'
-                name='userName'
-                value={form.userName}
-                placeholder='name'
+                name='email'
+                value={form.email}
                 onChange={onChangeHandler}
               />
-            )}
-            <input
-              type='text'
-              name='email'
-              value={form.email}
-              placeholder='Email'
-              onChange={onChangeHandler}
-            />
-            <input
-              type='password'
-              name='password'
-              value={form.password}
-              placeholder='Password'
-              onChange={onChangeHandler}
-            />
-            <input type='submit' value={isNewAccount ? "sign up" : "login"} />
-          </form>
+            </Box>
+            <Box
+              display='flex'
+              alignItems='center'
+              className={classes.passwordBox}
+            >
+              <Fingerprint />
+              <TextField
+                id='outlined-basic'
+                label='Password'
+                variant='outlined'
+                type='password'
+                name='password'
+                value={form.password}
+                onChange={onChangeHandler}
+              />
+            </Box>
+            <Button
+              variant='outlined'
+              className={classes.toggleButton1}
+              onClick={onSubmitHandler}
+            >
+              {isNewAccount ? "sign up" : "login"}
+            </Button>
+          </FormControl>
           <span>{authError}</span>
 
-          <div>
-            <span>
-              {!isNewAccount ? "First time here?" : "Good to see you back!"}
-            </span>
-            <button onClick={toggleAuthMethod}>
+          <Box display='flex' alignItems='center'>
+            <Typography variant='h6' className={classes.toggleMessage}>
+              {!isNewAccount ? "First time here?" : "Have we met before?"}
+            </Typography>
+            <Button
+              variant='outlined'
+              className={classes.toggleButton2}
+              onClick={toggleAuthMethod}
+            >
               {!isNewAccount ? "Sign Up" : "Log In"}
-            </button>
-          </div>
+            </Button>
+          </Box>
         </Paper>
       )}
     </>
@@ -110,3 +159,60 @@ function Auth(props) {
 }
 
 export default Auth;
+
+const useStyles = makeStyles({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "60px",
+    padding: "40px 0",
+  },
+  form: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: "600px",
+    margin: "40px auto 60px",
+    "& svg": {
+      color: "#98CDC6",
+    },
+  },
+  userNameBox: {
+    width: "100%",
+    marginBottom: "20px",
+    "& .MuiTextField-root": {
+      width: "100%",
+      marginLeft: "10px",
+    },
+  },
+  emailBox: {
+    width: "100%",
+    marginBottom: "20px",
+    "& .MuiTextField-root": {
+      width: "100%",
+      marginLeft: "10px",
+    },
+  },
+  passwordBox: {
+    width: "100%",
+    marginBottom: "20px",
+    "& .MuiTextField-root": {
+      width: "100%",
+      marginLeft: "10px",
+    },
+  },
+  toggleButton1: {
+    color: "#98CDC6",
+    fontSize: "1.05rem",
+  },
+  toggleButton2: {
+    color: "#CFB491",
+    fontSize: "0.8rem",
+  },
+  toggleMessage: {
+    marginRight: "15px",
+    fontSize: "1rem",
+    color: "#CFB491",
+  },
+});
